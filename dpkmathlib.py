15ba9ab74910a15600a98802d309a6f91a5b89c5
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-19 23:30:25 +0800

    remove test code

--- dpkmathlib.py
+++ dpkmathlib.py
@@ -1,7 +1,2 @@
 from function import *
 from vector import *
-
-if __name__ == '__main__':
-    f = Fx(lambda r, o: e**-r * sin(o))
-    print f.du(Vector(3,-2), 0, pi/3)
-    print 3/sqrt(13)*-sqrt(3)/2-2/sqrt(13)*1/2
