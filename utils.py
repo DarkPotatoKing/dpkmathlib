0bc2924882dcdd77a822c98bb1fcf335334fba2b
DarkPotatoKing <kyle.rosales97@gmail.com>
2015-12-19 23:51:23 +0800

    rename log as ln

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 # standard template for all classes
 from __future__ import division
 import math
-from math import e, sin, cos, pi, sqrt
+from math import e, sin, cos, pi, sqrt, log
 import copy
+
+ln = log
